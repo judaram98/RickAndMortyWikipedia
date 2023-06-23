d4f2724562752209b70d92e79540f6ce77d177fa
practicante3 <practicante@delphianalytics.co>
2023-06-23 15:11:09 -0500

    Pagination complete

--- src/App.js
+++ src/App.js
@@ -41,7 +41,11 @@ function App() {
           </div>
         </div>
       </div>
-      <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber} />
+      <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        setPageNumber={setPageNumber}
+      />
     </div>
   );
 }
