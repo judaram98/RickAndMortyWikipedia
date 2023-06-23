9da92e94df9ec4228f51fcdfc82cc75aa556f902
practicante3 <practicante@delphianalytics.co>
2023-06-23 11:09:58 -0500

    Data fetching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
 
 function App() {
+  const [pageNumber, setPageNumber] = useState(1);
+  const [fetchedData, setFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  useEffect(() => {
+    //Lets open an IIFE function to fetch data
+    (async function () {
+      //Now we are going to tell javascript that wait until fetching data from API and convert it into a json
+      let data = await fetch(api).then((res) => res.json());
+      setFetchedData(data);
+    })();
+  }, [api]);
+
   return (
     <div className="App">
       <h1 className="text-center ubuntu my-4">
